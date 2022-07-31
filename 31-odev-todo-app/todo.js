5ae6a6e08a0be73ab492f6a525533f205ada683c
navarcus <bspalamutcu@gmail.com>
2022-07-31 20:39:25 +0300

    odev guncellendi

--- 31-odev-todo-app/todo.js
+++ 31-odev-todo-app/todo.js
@@ -43,4 +43,4 @@ liste.addEventListener('click', function (e) {
 });
 /* Burada ise ul'ye event listener ekledik ve eventin hangi classi etkiledigine gore yapacagi islemleri belirledik. 2 ayri event listener ve fonksiyon olusturmak yerine tek event listener icinde 2 farkli fonksyion olusturmayi tercih ettim*/
 
-// iyi kodlamalar :) Berke Palamutcu tarafindan sevgiyle kodlandi <3 //
+//  Berke Palamutcu tarafindan  kodlandi //
